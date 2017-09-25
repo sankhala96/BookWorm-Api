cb97dee14f6fb8a8f9b2b5fd28decaf0832445c8
sankhala <sankhalayash96@gmail.com>
2017-09-25 20:02:44 +0530

    User auth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import express from 'express'
 import path from 'path'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import auth from './routes/auth'
+import dotenv from 'dotenv'
+import Promise from 'bluebird'
 
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
 
-app.post('/api/auth', (req, res) => {
-   res.status(400).json({
-       errors: { global: "Invalid credentials"}
-   });
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, function (err, res) {
+    if(err){
+        console.log('DB Connection Failed:' + err);
+    }
+    else{
+        console.log('DB Connection Success: ');
+    }
 });
+
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
