abd69c52989a3eae31d03d8cde381e1bb49b3b34
sankhala <sankhalayash96@gmail.com>
2017-10-07 21:00:13 +0530

    email confirmation

--- src/models/User.js
+++ src/models/User.js
@@ -7,10 +7,11 @@ const schema = new mongoose.Schema(
     {
         email: {type: String, required: true, lowercase: true, index: true, unique: true},
         passwordHash: {type: String, required: true},
-        confirmed: {type: Boolean, default: false}
+        confirmed: {type: Boolean, default: false},
+        confirmationToken: {type: String, default: ""}
     },
     {
-        timeStamp: true
+        timestamps: true
     }
 );
 
@@ -22,9 +23,18 @@ schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`
+};
+
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
-        email: this.email
+        email: this.email,
+        confirmed: this.confirmed
     },
         process.env.JWT_SECRET
     );
